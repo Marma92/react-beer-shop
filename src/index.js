967921a71e2ef3594fc33698af1d8885c889b21b
Amram ELBAZ <elbaz.amram@gmail.com>
2022-01-28 15:10:26 +0100

    fix: typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode, React } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
